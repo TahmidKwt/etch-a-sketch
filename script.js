b007cff2776c6d29a03e61d8984158c24c10f494
Coffin <100180424+TahmidKwt@users.noreply.github.com>
2023-09-25 05:00:58 +0600

    Create the last row with different class

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ const displayGrid = (columns, rows) => {
 		let column = document.createElement("div");
 		column.classList.add("column");
 
+	        let lastRow = document.createElement("div");  	
+		lastRow.classList.add("taken");  
+		lastRow.classList.add("row"); 
+		column.appendChild(lastRow);
+
 		for (let j = 0; j < rows; j++) {  
 		let row = document.createElement("div");
 		row.classList.add("row");
